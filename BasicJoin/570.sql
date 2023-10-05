8916e301b594505bec4c479686118546e2e54696
Gia-Long <58245859+T312@users.noreply.github.com>
2023-10-05 20:52:42 +0700

    Update 570.sql

--- BasicJoin/570.sql
+++ BasicJoin/570.sql
@@ -1,10 +1,19 @@
-select 
-  name 
-from 
-  Employee 
+select name 
+from Employee 
 where id in (
     select managerId from  Employee where id in 
         (select if( count(managerId) >= 5, id, null) from Employee 
         group by managerId
         )
   )
+  
+-- cach 2
+select name from Employee where id in (select managerId from Employee
+group by managerId 
+having count(managerId) >= 5)
+  
+-- cach 3
+select a.name 
+from Employee as a join Employee as b on a.id = b.managerId 
+group by b.managerId 
+having count(b.managerId) >= 5
